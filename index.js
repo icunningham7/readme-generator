d8f3384b3f6d7776fc3897a2ead715cf71b0ddee
Ian Cunningham <35973428+icunningham7@users.noreply.github.com>
2022-09-12 21:51:16 -0600

    inital commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    'What is your project title?',
+    'What was your motivation?',
+    'Why did you build this project?',
+    'What problem does it solve?',
+    'What makes this project standout'
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
